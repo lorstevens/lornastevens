0cfc26015e5529230b2f5b933bd30823899ea4d9
lorstevens <lornacstevens@gmail.com>
2018-01-05 14:34:22 -0600

    added modal

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+ $(document).ready(function(){
+ 	$(".ghost-button").click(function(){
+    $('#modal1').modal();
+     $('#modal1').modal('open');
+ 	})
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+
+  });
+       
