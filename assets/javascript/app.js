4a2bcb110aaddd554dc74d80d043066e67b8d3ad
lorstevens <lornacstevens@gmail.com>
2018-02-20 11:26:07 -0600

    updated css again to materialize

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,15 @@
 
 
 
-
+var $grid = $('.grid').masonry({
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  columnWidth: '.grid-sizer'
+});
+// layout Isotope after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry();
+});
 
 
 
